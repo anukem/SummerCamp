c4dea8c6b08dd778220daa64b2295cdc1a1cd5a6
Braxton Gunter <braxton.gun@gmail.com>
2016-11-22 02:15:58 -0500

    changes?

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,11 @@
 $(document).on('turbolinks:load', function() {
 
+	// if javascript is enabled; enable the AJAX form else default link to view
+	$('[data-link="register"]').click(function(event) {
+		// prevent default linkage
+		event.preventDefault();
+
+		$('.register-form-view').slideDown();
+	});
+
 });
